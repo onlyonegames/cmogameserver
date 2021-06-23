29748bb27c4d44893a1982983cb5dc6113a2bf4d
kaki462 <ukhwkyh5jp@privaterelay.appleid.com>
2021-06-23 16:33:07 +0900

    1. RedisReposirotyConfig수정

--- IdleGameServer/src/main/java/com/onlyonegames/eternalfantasia/config/RedisRepositoryConfig.java
+++ IdleGameServer/src/main/java/com/onlyonegames/eternalfantasia/config/RedisRepositoryConfig.java
@@ -1,7 +1,4 @@
 package com.onlyonegames.eternalfantasia.config;
-import com.onlyonegames.eternalfantasia.domain.model.dto.RdsScoreDto;
-import com.onlyonegames.eternalfantasia.domain.model.dto.ResponseDto.RankingInfoDto;
-import com.onlyonegames.eternalfantasia.domain.model.entity.Dungeon.Leaderboard.RedisScore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,16 +28,6 @@ public class RedisRepositoryConfig {
     public RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
-    }
-
-    @Bean
-    public RedisTemplate<String, RdsScoreDto> redisRdsScoreDtoTemplate() {
-        final RedisTemplate<String, RdsScoreDto> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer( new StringRedisSerializer() );
-        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(RdsScoreDto.class) );
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(RdsScoreDto.class) );
         return redisTemplate;
     }
 
