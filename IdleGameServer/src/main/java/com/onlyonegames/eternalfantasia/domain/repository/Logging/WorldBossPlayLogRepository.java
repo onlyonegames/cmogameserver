4fadc6c5082e1099d1124eda3c1056da6d2a496a
pupubaba <34086661+pupubaba@users.noreply.github.com>
2021-10-30 16:45:02 +0900

    1. 누락 코드 추가

--- IdleGameServer/src/main/java/com/onlyonegames/eternalfantasia/domain/repository/Logging/WorldBossPlayLogRepository.java
+++ IdleGameServer/src/main/java/com/onlyonegames/eternalfantasia/domain/repository/Logging/WorldBossPlayLogRepository.java
@@ -3,8 +3,10 @@ package com.onlyonegames.eternalfantasia.domain.repository.Logging;
 import com.onlyonegames.eternalfantasia.domain.model.entity.Logging.WorldBossPlayLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface WorldBossPlayLogRepository extends JpaRepository<WorldBossPlayLog, Long> {
     List<WorldBossPlayLog> findAllByUseridUser(Long userId);
+    List<WorldBossPlayLog> findAllByCreateddateAfter(LocalDateTime localDateTime);
 }
